0801ee75e2d9cd27ca77cc7b808b22d81ab85db9
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-09-17 16:15:46 +0530

    Learned Condition Types

--- conditional types/type Assertion.js
+++ conditional types/type Assertion.js
@@ -1,5 +1,5 @@
-var myStr = 'hello';
-console.log(myStr.length); // work
-var myStr2;
-myStr2 = 'hello2';
-console.log(myStr2.length); // work but it's type is any;
+// var myStr = 'hello';
+// console.log(myStr.length); // work
+// var myStr2;
+// myStr2 = 'hello2';
+// console.log(myStr2.length); // work but it's type is any;
